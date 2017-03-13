c835fe4aa36b5501cd5d4e507ea10cca0b6817c5
callee <240100889@qq.com>
2017-03-13 10:53:55 +0800

    1

--- rasterdemo.c
+++ rasterdemo.c
@@ -5,17 +5,12 @@
 
 #include <windows.h>
 #include <tchar.h>
-
 #include "gmath.h"
 #include "arithmetic.h"
 
 typedef unsigned int IUINT32;
 
-//=====================================================================
-// 渲染设备
-//=====================================================================
 typedef struct {
-	transform_t transform;      // 坐标变换器
 	int width;                  // 窗口宽度
 	int height;                 // 窗口高度
 	IUINT32 **framebuffer;      // 像素缓存：framebuffer[y] 代表第 y行
@@ -65,7 +60,6 @@ void device_init(device_t *device, int width, int height, void *fb) {
 	device->height = height;
 	device->background = 0xc0c0c0;
 	device->foreground = 0;
-	transform_init(&device->transform, width, height);
 	device->render_state = RENDER_STATE_WIREFRAME;
 }
 
@@ -76,20 +70,6 @@ void device_destroy(device_t *device) {
 	device->framebuffer = NULL;
 	device->zbuffer = NULL;
 	device->texture = NULL;
-}
-
-// 设置当前纹理
-void device_set_texture(device_t *device, void *bits, long pitch, int w, int h) {
-	char *ptr = (char*)bits;
-	int j;
-	assert(w <= 1024 && h <= 1024);
-	for (j = 0; j < h; ptr += pitch, j++) 	// 重新计算每行纹理的指针
-		device->texture[j] = (IUINT32*)ptr;
-	
-	device->tex_width = w;
-	device->tex_height = h;
-	device->max_u = (float)(w - 1);
-	device->max_v = (float)(h - 1);
 }
 
 // 清空 framebuffer 和 zbuffer
@@ -114,67 +94,6 @@ void device_pixel(device_t *device, int x, int y, IUINT32 color) {
 		device->framebuffer[y][x] = color;
 	}
 }
-
-// 绘制线段
-void device_draw_line(device_t *device, int x1, int y1, int x2, int y2, IUINT32 c) {
-	int x, y, rem = 0;
-	if (x1 == x2 && y1 == y2) {
-		device_pixel(device, x1, y1, c);
-	}	else if (x1 == x2) {
-		int inc = (y1 <= y2)? 1 : -1;
-		for (y = y1; y != y2; y += inc) device_pixel(device, x1, y, c);
-		device_pixel(device, x2, y2, c);
-	}	else if (y1 == y2) {
-		int inc = (x1 <= x2)? 1 : -1;
-		for (x = x1; x != x2; x += inc) device_pixel(device, x, y1, c);
-		device_pixel(device, x2, y2, c);
-	}	else {
-		int dx = (x1 < x2)? x2 - x1 : x1 - x2;
-		int dy = (y1 < y2)? y2 - y1 : y1 - y2;
-		if (dx >= dy) {
-			if (x2 < x1) x = x1, y = y1, x1 = x2, y1 = y2, x2 = x, y2 = y;
-			for (x = x1, y = y1; x <= x2; x++) {
-				device_pixel(device, x, y, c);
-				rem += dy;
-				if (rem >= dx) {
-					rem -= dx;
-					y += (y2 >= y1)? 1 : -1;
-					device_pixel(device, x, y, c);
-				}
-			}
-			device_pixel(device, x2, y2, c);
-		}	else {
-			if (y2 < y1) x = x1, y = y1, x1 = x2, y1 = y2, x2 = x, y2 = y;
-			for (x = x1, y = y1; y <= y2; y++) {
-				device_pixel(device, x, y, c);
-				rem += dx;
-				if (rem >= dy) {
-					rem -= dy;
-					x += (x2 >= x1)? 1 : -1;
-					device_pixel(device, x, y, c);
-				}
-			}
-			device_pixel(device, x2, y2, c);
-		}
-	}
-}
-
-// 根据坐标读取纹理
-IUINT32 device_texture_read(const device_t *device, float u, float v) {
-	int x, y;
-	u = u * device->max_u;
-	v = v * device->max_v;
-	x = (int)(u + 0.5f);
-	y = (int)(v + 0.5f);
-	x = CMID(x, 0, device->tex_width - 1);
-	y = CMID(y, 0, device->tex_height - 1);
-	return device->texture[y][x];
-}
-
-
-//=====================================================================
-// 渲染实现
-//=====================================================================
 
 // 绘制扫描线
 void device_draw_scanline(device_t *device, scanline_t *scanline) {
@@ -205,8 +124,8 @@ void device_draw_scanline(device_t *device, scanline_t *scanline) {
 				if (render_state & RENDER_STATE_TEXTURE) {
 					float u = scanline->v.tc.u * w;
 					float v = scanline->v.tc.v * w;
-					IUINT32 cc = device_texture_read(device, u, v);
-					framebuffer[x] = cc;
+					//IUINT32 cc = device_texture_read(device, u, v);
+					framebuffer[x] = 0xCCCCCC;
 				}
 			}
 		}
@@ -220,8 +139,7 @@ void device_render_trap(device_t *device, trapezoid_t *trap) {
 	scanline_t scanline;
 	int j, top, bottom;
 	top = (int)(trap->top + 0.5f);
-	bottom = (int)(trap->bottom + 0.5f);
-	//printf("top=%d, bottom=%d", top, bottom);
+	bottom = (int)(trap->bottom + 0.5f);	
 	for (j = top; j < bottom; j++) {
 		if (j >= 0 && j < device->height) {
 			trapezoid_edge_interp(trap, (float)j + 0.5f);
@@ -231,60 +149,6 @@ void device_render_trap(device_t *device, trapezoid_t *trap) {
 		if (j >= device->height) break;
 	}
 }
-
-// 根据 render_state 绘制原始三角形
-void device_draw_primitive(device_t *device, const vertex_t *v1, 
-	const vertex_t *v2, const vertex_t *v3) {
-	point_t p1, p2, p3, c1, c2, c3;
-	int render_state = device->render_state;
-
-	// 按照 Transform 变化
-	transform_apply(&device->transform, &c1, &v1->pos);
-	transform_apply(&device->transform, &c2, &v2->pos);
-	transform_apply(&device->transform, &c3, &v3->pos);
-
-	// 裁剪，注意此处可以完善为具体判断几个点在 cvv内以及同cvv相交平面的坐标比例
-	// 进行进一步精细裁剪，将一个分解为几个完全处在 cvv内的三角形
-	if (transform_check_cvv(&c1) != 0) return;
-	if (transform_check_cvv(&c2) != 0) return;
-	if (transform_check_cvv(&c3) != 0) return;
-
-	// 归一化
-	transform_homogenize(&device->transform, &p1, &c1);
-	transform_homogenize(&device->transform, &p2, &c2);
-	transform_homogenize(&device->transform, &p3, &c3);
-
-	// 纹理或者色彩绘制
-	if (render_state & (RENDER_STATE_TEXTURE | RENDER_STATE_COLOR)) {
-		vertex_t t1 = *v1, t2 = *v2, t3 = *v3;
-		trapezoid_t traps[2];
-		int n;
-
-		t1.pos = p1; 
-		t2.pos = p2;
-		t3.pos = p3;
-		t1.pos.w = c1.w;
-		t2.pos.w = c2.w;
-		t3.pos.w = c3.w;
-		
-		vertex_rhw_init(&t1);	// 初始化 w
-		vertex_rhw_init(&t2);	// 初始化 w
-		vertex_rhw_init(&t3);	// 初始化 w
-		
-		// 拆分三角形为0-2个梯形，并且返回可用梯形数量
-		n = trapezoid_init_triangle(traps, &t1, &t2, &t3);
-
-		if (n >= 1) device_render_trap(device, &traps[0]);
-		if (n >= 2) device_render_trap(device, &traps[1]);
-	}
-
-	if (render_state & RENDER_STATE_WIREFRAME) {		// 线框绘制
-		device_draw_line(device, (int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, device->foreground);
-		device_draw_line(device, (int)p1.x, (int)p1.y, (int)p3.x, (int)p3.y, device->foreground);
-		device_draw_line(device, (int)p3.x, (int)p3.y, (int)p2.x, (int)p2.y, device->foreground);
-	}
-}
-
 
 //=====================================================================
 // Win32 窗口及图形绘制：为 device 提供一个 DibSection 的 FB
@@ -414,80 +278,12 @@ void screen_update(void) {
 	screen_dispatch();
 }
 
-vertex_t mesh[] = {
-	{ {  1, -1,  1, 1 }, { 0, 0 }, { 1.0f, 0.2f, 0.2f }, 1 },
-	{ { -1, -1,  1, 1 }, { 0, 0.5f }, { 0.2f, 1.0f, 0.2f }, 1 },
-	{ { -1,  1,  1, 1 }, { 0.5f, 0.34f }, { 0.2f, 0.2f, 1.0f }, 1 },
-	{ {  1,  1,  1, 1 }, { 0, 0.34f }, { 1.0f, 0.2f, 1.0f }, 1 },
-	{ {  1, -1, -1, 1 }, { 0.5, 1 }, { 1.0f, 1.0f, 0.2f }, 1 },
-	{ { -1, -1, -1, 1 }, { 0, 1 }, { 0.2f, 1.0f, 1.0f }, 1 },
-	{ { -1,  1, -1, 1 }, { 0.5, 0.67f }, { 1.0f, 0.3f, 0.3f }, 1 },
-	{ {  1,  1, -1, 1 }, { 0, 0.67f }, { 0.2f, 1.0f, 0.3f }, 1 },
-};
-
-int meshIndexes[] = {0,1,2,2,3,0,  4,5,6,6,7,4, 0,4,5,5,1,0,  1,5,6,6,2,1,  2,6,7,7,3,2,  3,7,4,4,0,3};
-
-void draw_mesh(device_t *device, float theta) {
-	matrix_t m;
-	matrix_set_rotate(&m, -1, -0.5, 1, theta);
-	device->transform.world = m;
-	transform_update(&device->transform);
-	int cnt = sizeof(meshIndexes)/sizeof(int);
-	for (int i = 0; i < cnt; i+=3) {
-		vertex_t p1 = mesh[meshIndexes[i]], p2 = mesh[meshIndexes[i+1]], p3 = mesh[meshIndexes[i+2]];
-		device_draw_primitive(device, &p1, &p2, &p3);
-	}
-}
-
-void camera_at_zero(device_t *device, float x, float y, float z) {
-	point_t eye = { x, y, z, 1 }, at = { 0, 0, 0, 1 }, up = { 0, 0, 1, 1 };
-	matrix_set_lookat(&device->transform.view, &eye, &at, &up);
-	transform_update(&device->transform);
-}
-
-void read_ppm(char *filename, unsigned char** data, int* w, int* h){
-	char header[128];
-	FILE *pFile;
-	pFile = fopen(filename, "rb");
-	fgets(header, 128, pFile);	
-	printf("header=%s", header);	
-	fgets(header, 128, pFile);	
-	printf("header=%s", header);	
-	fgets(header, 128, pFile);
-	printf("header=%s\n", header);	
-	sscanf(header, "%d %d\n", w, h);
-	printf("w=%d,h=%d\n", *w, *h);
-	int cnt = (*w)*(*h)*3;
-	printf("cnt=%d\n", cnt);
-	*data = (unsigned char*)malloc(cnt);
-	fgets(header, 128, pFile);
-	fread(*data, cnt, 1, pFile);
-	printf("read ok");
-	fclose(pFile);
-}
-
-void init_texture(device_t *device) {
-	IUINT32 *texture;
-	unsigned char* texturedata;
-	int w=0, h=0;
-
-	read_ppm("x.ppm", &texturedata, &w, &h);	
-
-    texture = (IUINT32*)malloc(sizeof(IUINT32)*h*w);
-	
-	//拷贝数据到纹理数组
-	for (int j = 0; j < h; j++) {
-		int pitch = w*3*j;
-		for (int i = 0; i < w; i++) {
-			unsigned char r =  *(texturedata + pitch + i*3);
-			unsigned char g =  *(texturedata + pitch + i*3 + 1);
-			unsigned char b =  *(texturedata + pitch + i*3 + 2);
-			texture[j*w + i] = (r << 16) | (g << 8) | b;
+void drawline(device_t *device) {
+	for(int i=0;i<101;i++){
+		for(int j=0;j<101;j++){
+			device_pixel(device, j,i, 0xFFc0c0);
 		}
 	}
-
-	free(texturedata);
-	device_set_texture(device, texture, w*4, w, h);
 }
 
 int main(void)
@@ -506,35 +302,19 @@ int main(void)
 		return -1;
 
 	device_init(&device, 800, 600, screen_fb);
-	camera_at_zero(&device, 3, 0, 0);
-
-	init_texture(&device);
-	device.render_state = RENDER_STATE_TEXTURE;
-
+	
 	while (screen_exit == 0 && screen_keys[VK_ESCAPE] == 0) {
 		screen_dispatch();
 		device_clear(&device, 1);
-		camera_at_zero(&device, pos, 0, 0);
+		drawline(&device);
 		
-		if (screen_keys[VK_UP]) pos -= 0.01f;
-		if (screen_keys[VK_DOWN]) pos += 0.01f;
-		if (screen_keys[VK_LEFT]) alpha += 0.01f;
-		if (screen_keys[VK_RIGHT]) alpha -= 0.01f;
-
-		if (screen_keys[VK_SPACE]) {
-			if (kbhit == 0) {
-				kbhit = 1;
-				if (++indicator >= 3) indicator = 0;
-				device.render_state = states[indicator];
-			}
-		}	else {
-			kbhit = 0;
-		}
-
-		draw_mesh(&device, alpha);
+		trapezoid_t trap;
+		trap.top = 10;
+		trap.bottom = 150;
+		trap.left = {{},{},{}}
+		device_render_trap(&device, &trap);
 		screen_update();
 		Sleep(1);
 	}
 	return 0;
 }
-
